34b9e1be8eb4e62ac3df50b4e0ae8aca896f5b30
Kim sejun <semineun@naver.com>
2021-06-09 17:55:29 +0900

    21-06-09
    - 로그인 뷰 추가
    - 회원가입 뷰 추가
    - 인메모리 인증으로 admin 계정 로그인 테스트 성공

---
+++ src/main/java/com/cen/sec/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.cen.sec.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping(value = {"/", "/index"})
+    public String home(){
+        return "index";
+    }
+
+
+}
---
+++ src/main/java/com/cen/sec/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.cen.sec.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "/login";
+    }
+
+    @GetMapping("/signUp")
+    public String signUp(){
+        return "/signUp";
+    }
+
+}
